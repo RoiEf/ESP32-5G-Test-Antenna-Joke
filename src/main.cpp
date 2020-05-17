bc046fcd166a4b9e25468175501cb4cd6ade1c43
chaban2 <chaban2@myserver.com>
2020-05-17 12:07:07 +0300

    First working version. Basic functionality only.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 // #define DEBUGLEVEL 3
 #include <DebugUtils.h>  // used to simplyify debugging
 // ^^^ this line must be at the top
-#include <Arduino.h>            // Framework
-#include <DNSServer.h>          // AP Mode
-#include <ESPAsyncWebServer.h>  // Web server
-#include <ESPmDNS.h>            // Host name resolve
-#include <SPIFFS.h>             // files
-#include <WiFi.h>               // main WiFi library
+#include <Arduino.h>  // Framework
+// #include <DNSServer.h>          // AP Mode
+// #include <ESPAsyncWebServer.h>  // Web server
+// #include <ESPmDNS.h>            // Host name resolve
+// #include <SPIFFS.h>             // files
+#include <WiFi.h>  // main WiFi library
 
 #include <dwd.hpp>
 
@@ -27,8 +27,8 @@ IPAddress local_ip(192, 168, 0, 1);
 IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
 
-AsyncWebServer server(80);
-DNSServer dnsServer;
+// AsyncWebServer server(80);
+// DNSServer dnsServer;
 
 // void notFound(AsyncWebServerRequest *request);
 
@@ -45,28 +45,28 @@ void setup() {
     WiFi.softAPConfig(local_ip, gateway, subnet);
     DEBUGPRINTLN1(WiFi.softAPIP());
 
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
-        request->send(SPIFFS, "/index.html", "text/html");
-    });
-    server.on("/error", HTTP_GET, [](AsyncWebServerRequest* request) {
-        request->send(SPIFFS, "/error.html", "text/html");
-    });
-    server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest* request) {
-        request->send(SPIFFS, "/favicon.ico", "image/x-icon");
-    });
-    server.onNotFound([](AsyncWebServerRequest* request) {
-        request->send(SPIFFS, "/index.html", "text/html");
-    });
+    // server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    //     request->send(SPIFFS, "/index.html", "text/html");
+    // });
+    // server.on("/error", HTTP_GET, [](AsyncWebServerRequest* request) {
+    //     request->send(SPIFFS, "/error.html", "text/html");
+    // });
+    // server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest* request) {
+    //     request->send(SPIFFS, "/favicon.ico", "image/x-icon");
+    // });
+    // server.onNotFound([](AsyncWebServerRequest* request) {
+    //     request->send(SPIFFS, "/index.html", "text/html");
+    // });
 
-    server.begin();
-    DEBUGPRINTLN1("HTTP server started");
+    // server.begin();
+    // DEBUGPRINTLN1("HTTP server started");
 
-    if (!MDNS.begin(Host_Name)) {  //http://5G_Test_Antenna.local
-        DEBUGPRINTLN1("Error setting up MDNS responder!");
-    } else {
-        DEBUGPRINTLN1("Created local DNS");
-        MDNS.addService("http", "tcp", 80);
-    }
+    // if (!MDNS.begin(Host_Name)) {  //http://5G_Test_Antenna.local
+    //     DEBUGPRINTLN1("Error setting up MDNS responder!");
+    // } else {
+    //     DEBUGPRINTLN1("Created local DNS");
+    //     MDNS.addService("http", "tcp", 80);
+    // }
 
     delay(5000);
 }
